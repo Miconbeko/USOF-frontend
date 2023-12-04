ab9a7cc48f3771d47b3f5002afe4a2c0c86d1d2b
Miconbeko <stepanov.m2004@gmail.com>
2023-12-04 18:08:33 +0200

    fix: lock comment buttons on locked post

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,8 +4,11 @@ import { useState } from "react";
 import CommentForm from "./forms/CommentForm";
 import { nanoid } from "@reduxjs/toolkit";
 import ConfirmButton from "./buttons/ConfirmButton";
+import { useSelector } from "react-redux";
+import { getPostLock } from "../store/slices/postSlice";
 
 export default function Comment({ comment, onCommentAdd }) {
+	const postLock = useSelector(getPostLock);
 	const [showCommnetForm, setShowCommentForm] = useState(false);
 
 	const handleShowForm = async () => {
@@ -21,7 +24,11 @@ export default function Comment({ comment, onCommentAdd }) {
 		<div>
 			<p>{comment.content}</p>
 			<RequireAuthComponents>
-				<ConfirmButton actionHandler={handleShowForm} withoutWarning>
+				<ConfirmButton
+					actionHandler={handleShowForm}
+					withoutWarning
+					locked={Boolean(postLock)}
+				>
 					Comment
 				</ConfirmButton>
 			</RequireAuthComponents>
